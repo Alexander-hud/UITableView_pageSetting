f520eb5c3f11b142a5f99662faabb4118bde38da
Mogan <krsk.1994@mail.ru>
2022-07-03 12:47:03 +0400

    Initial Commit

---
+++ UITableView_pageSetting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView_pageSetting
+//
+//  Created by Александр Алешин on 03.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
