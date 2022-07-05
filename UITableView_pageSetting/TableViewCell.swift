e6a4e7d6dc899387645d8c44c73707cb98db234f
Mogan <krsk.1994@mail.ru>
2022-07-05 19:32:29 +0400

    test

---
+++ UITableView_pageSetting/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  UITableView_pageSetting
+//
+//  Created by Александр Алешин on 05.07.2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    static let id = "TableViewCell"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
